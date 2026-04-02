c8716d89a7ee8f1a60dfa1455f99f44fb66c59c4
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2026-04-02 04:12:48 +0200

    Validator terminado

--- src/main/java/com/trinitarias/proyecto_tfg_api/controller/TfgController.java
+++ src/main/java/com/trinitarias/proyecto_tfg_api/controller/TfgController.java
@@ -8,6 +8,7 @@ import com.trinitarias.proyecto_tfg_api.validator.TfgValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,20 +27,24 @@ public class TfgController {
      * @return
      */
     @PostMapping
-    public ResponseEntity<?> crearUsuario(@RequestBody TfgUsuariosDto tfgUsuariosDto) {
+    public ResponseEntity<?> crearUsuario(@RequestBody TfgUsuariosDto tfgUsuariosDto, Errors error) {
         HttpStatus status = null;
         Object body = null;
 
-
-        TfgUsuariosDto dto = service.crearUsuario(tfgUsuariosDto);
-        if (tfgUsuariosDto != null) {
-            status = HttpStatus.CREATED;
-            body = dto;
-        } else {
+        validator.validate(tfgUsuariosDto, error);
+        if (!error.hasErrors()) {
+            TfgUsuariosDto dto = service.crearUsuario(tfgUsuariosDto);
+            if (tfgUsuariosDto != null) {
+                status = HttpStatus.CREATED;
+                body = dto;
+            }else {
+                status = HttpStatus.BAD_REQUEST;
+                body = null;
+            }
+        }else{
             status = HttpStatus.BAD_REQUEST;
-            body = null;
+            body = error.getAllErrors();
         }
-
         return ResponseEntity.status(status).body(body);
     }
 
@@ -83,17 +88,23 @@ public class TfgController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody TfgUsuariosDto tfgUsuariosDto){
+    public ResponseEntity<?> login(@RequestBody TfgUsuariosDto tfgUsuariosDto, Errors error){
         HttpStatus status = null;
         Object body = null;
 
-        TfgUsuariosDto dto = service.login(tfgUsuariosDto);
-        if (dto != null) {
-            status = HttpStatus.OK;
-            body = dto;
-        } else{
+        validator.validate(tfgUsuariosDto, error);
+        if (!error.hasErrors()) {
+            TfgUsuariosDto dto = service.login(tfgUsuariosDto);
+            if (dto != null) {
+                status = HttpStatus.OK;
+                body = dto;
+            }else {
+                status = HttpStatus.BAD_REQUEST;
+                body = null;
+            }
+        } else {
             status = HttpStatus.BAD_REQUEST;
-            body = null;
+            body = error.getAllErrors();
         }
 
         return ResponseEntity.status(status).body(body);
--- src/main/java/com/trinitarias/proyecto_tfg_api/validator/TfgValidator.java
+++ src/main/java/com/trinitarias/proyecto_tfg_api/validator/TfgValidator.java
@@ -1,5 +1,8 @@
 package com.trinitarias.proyecto_tfg_api.validator;
 
+import com.trinitarias.proyecto_tfg_api.dto.TfgUsuariosDto;
+import com.trinitarias.proyecto_tfg_api.service.TfgService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -7,6 +10,8 @@ import org.springframework.validation.Validator;
 @Component
 public class TfgValidator implements Validator {
 
+    @Autowired
+    TfgService service;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -15,6 +20,24 @@ public class TfgValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+        TfgUsuariosDto dto = (TfgUsuariosDto) target;
 
+        if(dto.getNombre_usuario() == null || dto.getNombre_usuario().isBlank()) {
+            errors.rejectValue("usuarios.usuario","i18n.usuario.obligatorio" ,"El usuario es obligatorio");
+        }
+        if(dto.getContrasena() == null || dto.getContrasena().isBlank()) {
+            errors.rejectValue("usuarios.contrasena","i18n.contrasena.obligatorio" ,"La contraseña es obligatoria");
+        }
+        if(!dto.getNombre_usuario().isEmpty() && !dto.getContrasena().isEmpty()) {
+            TfgUsuariosDto dtoU = service.login(dto);
+            if(dtoU==null) {
+                errors.rejectValue("usuarios.usuario","i18n.usuario.login" ,"El usuario y/o la contraseña son incorrectos");
+            }
+        }
+        if (dto.getEmail().isBlank()) {
+            errors.rejectValue("usuarios.email", "i18n.email.obligatorio", "El campo debe ser obligatorio");
+        } else if(dto.getEmail().indexOf('@')<0 || dto.getEmail().indexOf('.')<0 || dto.getEmail().indexOf('@')>dto.getEmail().indexOf('.')) {
+            errors.rejectValue("usuarios.email", "i18n.email.formato", "El campo debe ser un email");
+        }
     }
 }
