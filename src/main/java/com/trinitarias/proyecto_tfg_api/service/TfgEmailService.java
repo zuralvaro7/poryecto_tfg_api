3f3e2d2a78e2d946cf95a3102bdb776795c057ce
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2026-06-10 23:11:26 +0200

    pruebas logs

--- src/main/java/com/trinitarias/proyecto_tfg_api/service/TfgEmailService.java
+++ src/main/java/com/trinitarias/proyecto_tfg_api/service/TfgEmailService.java
@@ -13,15 +13,45 @@ public class TfgEmailService {
     private JavaMailSender mailSender;
 
 
+    @Value("${spring.mail.username}")
+    private String from;
 
+
+//    public void sendVerificationEmail(String to, String token) {
+//        SimpleMailMessage mensaje = new SimpleMailMessage();
+//        mensaje.setTo(to);
+//        mensaje.setFrom(from);
+//        mensaje.setSubject("Verificación de Cuenta");
+//        mensaje.setText("Por favor, verifica tu cuenta: https://poryectotfgapi-production.up.railway.app/api/v1/tfg/verificar?token=" + token);
+//        mailSender.send(mensaje);
+//
+//    }
 
     public void sendVerificationEmail(String to, String token) {
-        SimpleMailMessage mensaje = new SimpleMailMessage();
-        mensaje.setTo(to);
-        mensaje.setFrom("norepli@tfgapi.com");
-        mensaje.setSubject("Verificación de Cuenta");
-        mensaje.setText("Por favor, verifica tu cuenta: https://poryectotfgapi-production.up.railway.app/api/v1/tfg/verificar?token=" + token);
-        mailSender.send(mensaje);
 
+        System.out.println("=== INICIO ENVIO EMAIL ===");
+        System.out.println("DESTINO: " + to);
+
+        SimpleMailMessage mensaje = new SimpleMailMessage();
+
+        mensaje.setTo(to);
+        mensaje.setFrom("TU_CORREO_GMAIL@gmail.com");
+        mensaje.setSubject("Verificación de Cuenta");
+
+        mensaje.setText(
+                "https://poryectotfgapi-production.up.railway.app/api/v1/tfg/verificar?token="
+                        + token
+        );
+
+        try {
+            mailSender.send(mensaje);
+            System.out.println("EMAIL ENVIADO CORRECTAMENTE");
+        } catch (Exception e) {
+            System.out.println("ERROR ENVIANDO EMAIL");
+            e.printStackTrace();
+        }
+
+        System.out.println("=== FIN ENVIO EMAIL ===");
     }
+
 }
