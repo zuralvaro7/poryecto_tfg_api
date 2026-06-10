e4acb045be1c732416b9eeeb37f75a4433aa4666
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2026-06-11 00:01:18 +0200

    Merge branch 'main' of https://github.com/zuralvaro7/poryecto_tfg_api
    # Conflicts:
    #	src/main/java/com/trinitarias/proyecto_tfg_api/service/TfgEmailService.java

--- src/main/java/com/trinitarias/proyecto_tfg_api/service/TfgEmailService.java
+++ src/main/java/com/trinitarias/proyecto_tfg_api/service/TfgEmailService.java
@@ -1,6 +1,7 @@
 package com.trinitarias.proyecto_tfg_api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,13 @@ public class TfgEmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${API}")
     public void sendVerificationEmail(String to, String token) {
         SimpleMailMessage mensaje = new SimpleMailMessage();
         mensaje.setTo(to);
         mensaje.setFrom("norepli@tfgapi.com");
         mensaje.setSubject("Verificación de Cuenta");
+        mensaje.setText("Por favor, verifica tu cuenta: "+ api +"/api/v1/tfg/verificar?token=" + token);
         mensaje.setText("Por favor, verifica tu cuenta: ${PAGINA}/api/v1/tfg/verificar?token=" + token);
         mailSender.send(mensaje);
 
